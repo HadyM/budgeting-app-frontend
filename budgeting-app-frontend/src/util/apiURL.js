9f6781d110eb22cea35b0dafc5fd0f0f74288331
Hady Mohamed <hadymohamed@pursuit.org>
2021-06-25 00:14:34 -0400

    fixed backend link to the frontend

--- budgeting-app-frontend/src/util/apiURL.js
+++ budgeting-app-frontend/src/util/apiURL.js
@@ -1,5 +1,5 @@
 export const apiURL = () => {
   return window.location.hostname === "localhost"
     ? "http://localhost:3000"
-    : "https://sheltered-island-89188.herokuapp.com";
+    : "https://budgeting-app-backend-api.herokuapp.com/";
 };
