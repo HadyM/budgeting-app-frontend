6153739dbe401a897752478cfe90855bd9b4852d
Hady Mohamed <hadymohamed@pursuit.org>
2021-06-25 12:35:29 -0400

    removed backslash

--- budgeting-app-frontend/src/util/apiURL.js
+++ budgeting-app-frontend/src/util/apiURL.js
@@ -1,5 +1,5 @@
 export const apiURL = () => {
   return window.location.hostname === "localhost"
     ? "http://localhost:3003"
-    : "https://budgeting-app-backend-api.herokuapp.com/";
+    : "https://budgeting-app-backend-api.herokuapp.com";
 };
